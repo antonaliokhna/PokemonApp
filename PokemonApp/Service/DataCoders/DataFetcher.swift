04a7e127c8eb6a1755575f56c9d70ee79ae87160
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 19:22:14 +0300

    Create data fetcher (decodable class) in service

--- PokemonApp/Service/DataCoders/DataFetcher.swift
+++ PokemonApp/Service/DataCoders/DataFetcher.swift
@@ -9,4 +9,39 @@ import Foundation
 
 class DataFetcher {
 
+    private let service: AlamofireNetworkService
+
+    init(service: AlamofireNetworkService) {
+        self.service = service
+    }
+
+    func fetchGenericData<T: Decodable>(
+        url: String,
+        parameters: [String: Any]
+    ) async throws -> T {
+
+        let data = try await service.fetch(
+            from: url,
+            parameters: parameters
+        )
+
+        return try self.decodeData(type: T.self, from: data)
+    }
+
+    private func decodeData<T: Decodable>(
+        type: T.Type,
+        from: Data?
+    ) throws -> T {
+        guard let data = from else {
+            throw CustomError.codableError(error: .dataIsEmpty)
+        }
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        guard let object = try? decoder.decode(type, from: data) else {
+            throw CustomError.codableError(error: .decodingDataFailed)
+        }
+
+        return object
+    }
 }
