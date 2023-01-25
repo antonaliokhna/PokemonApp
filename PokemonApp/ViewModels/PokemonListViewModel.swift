b0baf5289cd5a4fc92012bc5e670dd00112a8eab
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 12:56:51 +0300

    Add logic to 'pokemon view model' and add view model property in view (simple MVVM)

--- PokemonApp/ViewModels/PokemonListViewModel.swift
+++ PokemonApp/ViewModels/PokemonListViewModel.swift
@@ -9,4 +9,31 @@ import Foundation
 
 class PokemonListViewModel: ObservableObject {
 
+    @Published var pokemonModels: [PokemonPreviewListModel] = []
+
+    init() {
+        //empty init...
+    }
+
+    func loadPokemons() {
+        pokemonModels = [
+            .init(name: "first", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "second", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "last", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "first", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "second", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+            .init(name: "last", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
+        ]
+    }
+
 }
--- PokemonApp/Views/PokemonListScreen/PokemonListView.swift
+++ PokemonApp/Views/PokemonListScreen/PokemonListView.swift
@@ -9,30 +9,12 @@ import SwiftUI
 
 struct PokemonListView: View {
 
-    //MARK: For test...
-    let models: [PokemonPreviewListModel] = [
-        .init(name: "first", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "second", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "last", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "first", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "second", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "test", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-        .init(name: "last", url: URL(string: "https://pokeapi.co/api/v2/pokemon/10263/")!),
-    ]
+    @StateObject private var viewModel = PokemonListViewModel()
 
     var body: some View {
         NavigationView {
             VStack {
-                List(models) { model in
+                List(viewModel.pokemonModels) { model in
                     NavigationLink {
                         //TODO: For test
                         PokemonListView()
@@ -61,6 +43,9 @@ struct PokemonListView: View {
 
             }
             .navigationTitle("Pokemons")
+        }
+        .onAppear {
+            viewModel.loadPokemons()
         }
     }
 }
