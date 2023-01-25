2505834d2b7072d4cc540f3662ad873279303ef7
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 16:01:14 +0300

    Create custom network error enum

---
+++ PokemonApp/Service/Helpers/Errors/NetworkError.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkError.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import Foundation
+
+enum NetworkError: Error, LocalizedError {
+    case requestFailed(statusCode: Int?)
+    case internetConnectionFailed
+    case requestTimeOut
+    case invalidURL
+
+    var errorDescription: String! {
+        let description: String
+        switch self {
+        case .requestFailed(let statusCode):
+            description = "Request execution error \(statusCode ?? 418)."
+        case .invalidURL:
+            description = "Error when trying to execute a link request. Please contact the developer."
+        case .requestTimeOut:
+            description = "The request has exceeded the time allowed. Please try again later."
+        case .internetConnectionFailed:
+            description = "There is no internet connection. Please check the connection and restart the application."
+        }
+
+        return description
+    }
+}
