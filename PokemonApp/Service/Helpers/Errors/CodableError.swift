9ce8aaa59ba26be11ee38d7668336bc2b2d48559
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 15:56:29 +0300

    Create custom codable error enum

---
+++ PokemonApp/Service/Helpers/Errors/CodableError.swift
@@ -0,0 +1,29 @@
+//
+//  CodableError.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import Foundation
+
+enum CodableError: Error, LocalizedError{
+    case decodingDataFailed
+    case encodingDataFailed
+    case dataIsEmpty
+
+    var errorDescription: String! {
+        var description: String
+        switch self {
+        case .decodingDataFailed:
+            description = "Data decoding failed, "
+        case .encodingDataFailed:
+            description = "Data encoding failed, "
+        case .dataIsEmpty:
+            description = "Data is empty, "
+        }
+        description += "please contact the developer, or try again later..."
+
+        return description
+    }
+}
