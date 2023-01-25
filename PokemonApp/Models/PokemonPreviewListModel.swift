99d253ec62e5be9e792937a5234fcc3208924b94
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 12:12:57 +0300

    Add pokemon preview list model in 'Model' folder

---
+++ PokemonApp/Models/PokemonPreviewListModel.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonPreviewListModel.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import Foundation
+
+struct PokemonPreviewListModel {
+    let name: String
+    let url: URL
+}
