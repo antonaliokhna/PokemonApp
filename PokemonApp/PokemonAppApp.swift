557d06315b9a66a291af38fdeea4d99d68113c13
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 11:54:40 +0300

    Init commit (create empty project)

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
