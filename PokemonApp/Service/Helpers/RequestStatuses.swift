e499b425c4e4c05984b8f9d69939c62345242b53
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 15:49:53 +0300

    Add 'RequestStatuses' enum in service

---
+++ PokemonApp/Service/Helpers/RequestStatuses.swift
@@ -0,0 +1,14 @@
+//
+//  RequestStatuses.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import Foundation
+
+enum RequestStatuses {
+    case failed
+    case sucsess
+    case loading
+}
