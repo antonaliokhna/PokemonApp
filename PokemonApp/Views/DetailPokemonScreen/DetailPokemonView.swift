1e6512c845a30443fb114c6dad745c94407d04f6
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 13:19:20 +0300

    Create empty detail pokemon view

---
+++ PokemonApp/Views/DetailPokemonScreen/DetailPokemonView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailPokemonView.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import SwiftUI
+
+struct DetailPokemonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailPokemonView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPokemonView()
+    }
+}
