8f1eb925a8a0afc8ded56bcd5b1a574eec1bfebf
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-25 16:33:19 +0300

    Add AFError extension(conversion to CustomError type)

---
+++ PokemonApp/Extensions/AFError + extensions.swift
@@ -0,0 +1,27 @@
+//
+//  AFError + extensions.swift
+//  PokemonApp
+//
+//  Created by Anton Aliokhna on 1/25/23.
+//
+
+import Foundation
+import Alamofire
+
+extension AFError {
+    var convertToNetworkError: NetworkError {
+        let networkError: NetworkError
+        switch self {
+        case .invalidURL:
+            networkError = .invalidURL
+        case .sessionTaskFailed(URLError.notConnectedToInternet):
+            networkError = .internetConnectionFailed
+        case .sessionTaskFailed(URLError.timedOut):
+            networkError = .requestTimeOut
+        default:
+            networkError = .requestFailed(statusCode: self.responseCode)
+        }
+
+        return networkError
+    }
+}
