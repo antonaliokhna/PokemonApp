db0393a3fac70aa83e18b8f20b67d8530b7bb076
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-28 14:26:28 +0300

    Add test for async load data method in PokemonListViewModelTests

--- PokemonAppTests/ViewModels/PokemonListViewModelTests.swift
+++ PokemonAppTests/ViewModels/PokemonListViewModelTests.swift
@@ -10,15 +10,18 @@ import XCTest
 
 final class PokemonListViewModelTests: XCTestCase {
     var sut: PokemonListViewModel!
+    var mockNetworkService: MockNetworkDataService!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
 
-        sut = PokemonListViewModel()
+        mockNetworkService = MockNetworkDataService()
+        sut = PokemonListViewModel(networkService: mockNetworkService)
     }
 
     override func tearDownWithError() throws {
         sut = nil
+        mockNetworkService = nil
 
         try super.tearDownWithError()
     }
@@ -40,6 +43,25 @@ final class PokemonListViewModelTests: XCTestCase {
         sut.switchPage(side: .left)
 
         XCTAssert(sut.currentPage == result, "The page was not switched to the left!")
+    }
+
+    func testLoadPokemonsDataFromMockNetworkService() async throws {
+        let resultCount = 5
+        let resultStatus = RequestStatuses.sucsess
+
+        await sut.loadPokemonsData()
+
+        XCTAssert(
+            mockNetworkService.fetchPreviewPokemonListByPageCalled,
+                  "The mock download method has not been executed"
+        )
+        XCTAssert(sut.status == resultStatus,
+                  "The method of loading pokemon ended in an error"
+        )
+        XCTAssert(
+            sut.pokemonModel.count == resultCount,
+            "The number of pokemon obtained is not equal to 5"
+        )
     }
 
 }
