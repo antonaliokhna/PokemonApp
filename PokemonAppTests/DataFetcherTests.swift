d0e857a52f83ef7b9504319eb858b4a71d7f83df
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-29 02:56:39 +0300

    Add dafa fetcher tests

--- PokemonAppTests/DataFetcherTests.swift
+++ PokemonAppTests/DataFetcherTests.swift
@@ -10,30 +10,131 @@ import XCTest
 
 final class DataFetcherTests: XCTestCase {
     var sut: DataFetcher!
+    var mockService: MockDataFetcherService!
 
     override func setUpWithError() throws {
         try super.setUpWithError()
+
+        mockService = MockDataFetcherService()
+        sut = DataFetcher(service: mockService)
     }
 
     override func tearDownWithError() throws {
         sut = nil
+        mockService = nil
 
         try super.tearDownWithError()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFetchGenericDataMethodCalled() async {
+        do {
+            let _: PokemonListModel =
+            try await sut.fetchGenericData(
+                url: "foo",
+                parameters: [:]
+            )
+        } catch {
+            //Empty
+        }
+
+        XCTAssertTrue(
+            mockService.fetchCalled,
+            "Method fetch doesn't called when fetchGenericData executed"
+        )
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFetchGenericDataReturnDecodingDataFailedError() async {
+        mockService.returnData = Data(count: 0)
+
+        do {
+            let _: Data =
+            try await sut.fetchGenericData(
+                url: "foo",
+                parameters: [:]
+            )
+        } catch {
+            guard case CustomError.codableError(
+                error: .decodingDataFailed
+            ) = error else {
+                XCTFail("FetchGenericData returns another error")
+
+                return
+            }
         }
     }
 
+    func testCorrectFetchPokemonListData() async throws {
+        let pokemonList = PokemonListModel()
+        guard let pokemonData =
+                try? JSONEncoder().encode(pokemonList) else {
+            XCTFail("Encode PokemonListData failed")
+
+            return
+        }
+        mockService.returnData = pokemonData
+
+        let resultPokemonList: PokemonListModel =
+        try await sut.fetchGenericData(
+            url: "foo",
+            parameters: [:]
+        )
+
+        XCTAssert(
+            pokemonList.count == resultPokemonList.count,
+            "Incorrect data decoding (count)"
+        )
+        XCTAssert(
+            pokemonList.results.count == resultPokemonList.results.count,
+            "Incorrect data decoding (returl count)"
+        )
+    }
+
+    func testCorrectFetchDefailPokemonListData() async throws {
+        let detailPokemonModel = DetailPokemonModel(
+            name: "charizard",
+            height: 17,
+            weight: 905,
+            isDefault: true,
+            sprites: .init(frontDefault: nil),
+            types: [
+                .init(type: .init(name: "fire")),
+                .init(type: .init(name: "flying")),
+            ],
+            abilities: [
+                .init(ability: .init(name: "blaze")),
+                .init(ability: .init(name: "solar-power")),
+            ]
+        )
+
+        guard let detailPokemonData =
+                try? JSONEncoder().encode(detailPokemonModel) else {
+            XCTFail("Encode DetailPokemonModel failed")
+
+            return
+        }
+        mockService.returnData = detailPokemonData
+
+        let resultDetailPokemonModel: DetailPokemonModel =
+        try await sut.fetchGenericData(
+            url: "foo",
+            parameters: [:]
+        )
+
+        XCTAssert(
+            detailPokemonModel.name == resultDetailPokemonModel.name,
+            "Name not equal \(detailPokemonModel.name) after decoding"
+        )
+        XCTAssert(
+            detailPokemonModel.weight == resultDetailPokemonModel.weight,
+            "Weight not equal \(detailPokemonModel.weight) after decoding"
+        )
+        XCTAssert(
+            detailPokemonModel.height == resultDetailPokemonModel.height,
+            "Height not equal \(detailPokemonModel.height) after decoding"
+        )
+        XCTAssert(
+            detailPokemonModel.isDefault == resultDetailPokemonModel.isDefault,
+            "'default' not equal \(detailPokemonModel.isDefault) after decoding"
+        )
+    }
 }
