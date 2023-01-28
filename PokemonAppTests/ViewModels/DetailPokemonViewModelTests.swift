2b788aaa3cb2b647d8ce1696e97f8d5f9a2e089d
Anton Aliokhna <antonaliokhna@gmail.com>
2023-01-28 15:31:07 +0300

    Add tests for DetailPokemonViewModelTests

--- PokemonAppTests/ViewModels/DetailPokemonViewModelTests.swift
+++ PokemonAppTests/ViewModels/DetailPokemonViewModelTests.swift
@@ -32,6 +32,41 @@ final class DetailPokemonViewModelTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    
+    func testChangeRequestStatusWhenReloadPokemonData() {
+        let resultRequestStatus = RequestStatuses.loading
+
+        sut.reloadData()
+
+        XCTAssert(
+            sut.status == resultRequestStatus,
+            "Request status has not changed to 'loading'"
+        )
+    }
+
+    func testLoadPokemonDataFromMockNetworkService() async {
+        let resultStatus = RequestStatuses.sucsess
+
+        await sut.loadPokemonData()
+
+        XCTAssert(
+            mockNetworkService.fetchDetailPokemonByUrlCalled,
+            "The mock download method has not been executed"
+        )
+        XCTAssert(
+            sut.status == resultStatus,
+            "The method of loading pokemon ended in an error"
+        )
+    }
+
+    func testShowImageBlockReturnFalseWhenImageLinkEqualNil() {
+        let result = false
+
+        let sutResult = sut.showImageBlock
+
+        XCTAssert(
+            sutResult == result,
+            "Show image block variable returns true when image link is nil"
+        )
+    }
 
 }
